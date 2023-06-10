c5071c0feea62aee7aa86d1d94702f0d0f5fe392
Nrosy10 <117885685+Nrosy10@users.noreply.github.com>
2023-06-10 09:09:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function getBrowerinfo(){
+	let browerNm=navigator.appName;
+	let version=navigator.appVersion;
+	let message= "You are using"+" "+browerNm+" "+"version"+version;
+	let infoDiv=document.getElementById('browser-info');
+	infoDiv.textContent=message;
+	
+}
+window.onload=getBrowerinfo();
